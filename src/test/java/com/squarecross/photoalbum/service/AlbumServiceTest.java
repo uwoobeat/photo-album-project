b88cdff41755b177e8f67870cb28d18ab266d93e
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-03-15 18:45:36 +0900

    전체 앨범 가져오기 테스트

--- src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/squarecross/photoalbum/service/AlbumServiceTest.java
@@ -1,10 +1,14 @@
 package com.squarecross.photoalbum.service;
 
 import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.repository.AlbumRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,8 +39,21 @@ class AlbumServiceTest {
     }
 
     @Test
+    @DisplayName("전체 앨범 가져오기 테스트")
     void getAllAlbums() {
+        // given
+        Album album1 = Album.createAlbum("testAlbum1");
+        Album album2 = Album.createAlbum("testAlbum2");
+        em.persist(album1);
+        em.persist(album2);
+
+        // when
+        int albumCount = albumService.getAllAlbums().size();
+
+        // then
+        assertEquals(2, albumCount);
     }
+
     @Test
     void findAlbumsById() {
     }
