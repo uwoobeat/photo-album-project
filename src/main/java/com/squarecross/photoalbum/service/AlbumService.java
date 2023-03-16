80dd91db26007404a7dc775c609ffd73cefae034
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-03-16 14:19:14 +0900

    성능 최적화를 위해 읽기 전용 트랜잭션으로 수정

--- src/main/java/com/squarecross/photoalbum/service/AlbumService.java
+++ src/main/java/com/squarecross/photoalbum/service/AlbumService.java
@@ -3,10 +3,12 @@ package com.squarecross.photoalbum.service;
 import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.repository.AlbumRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class AlbumService {
     private final AlbumRepository albumRepository;
 
@@ -18,10 +20,12 @@ public class AlbumService {
         return albumRepository.save(album);
     }
 
+    @Transactional(readOnly = true)
     public List<Album> getAllAlbums() {
         return albumRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public List<Album> findAlbumsByName(String name) {
         return albumRepository.findByName(name);
     }
