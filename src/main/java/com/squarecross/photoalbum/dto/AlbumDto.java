f25fc7910777ead40c205552fdb31437d04f7421
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-03-17 19:44:32 +0900

    모든 필드에 대한 유효성 검증 추가

--- src/main/java/com/squarecross/photoalbum/dto/AlbumDto.java
+++ src/main/java/com/squarecross/photoalbum/dto/AlbumDto.java
@@ -1,17 +1,24 @@
 package com.squarecross.photoalbum.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AlbumDto {
+    @NotNull
     private Long id;
+    @NotBlank
     private String name;
+    @PastOrPresent
     private Date createdAt;
+    @Size(min = 0)
     private int photoCount;
 }
