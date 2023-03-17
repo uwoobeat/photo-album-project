a2a9e84f8347453af339fed96a7489fbdfe8e995
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-03-17 19:45:10 +0900

    AlbumMapper 생성

---
+++ src/main/java/com/squarecross/photoalbum/mapper/AlbumMapper.java
@@ -0,0 +1,25 @@
+package com.squarecross.photoalbum.mapper;
+
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.dto.AlbumDto;
+
+public class AlbumMapper {
+
+    public Album toEntity(AlbumDto albumDto) {
+        return Album.dtoBuilder()
+                .id(albumDto.getId())
+                .name(albumDto.getName())
+                .createdAt(albumDto.getCreatedAt())
+                .photoCount(albumDto.getPhotoCount())
+                .build();
+    }
+
+    public AlbumDto toDto(Album album) {
+        return AlbumDto.builder()
+                .id(album.getId())
+                .name(album.getName())
+                .createdAt(album.getCreatedAt())
+                .photoCount(album.getPhotoCount())
+                .build();
+    }
+}
