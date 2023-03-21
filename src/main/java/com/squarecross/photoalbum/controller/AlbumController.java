c4e69c42a51fc5e6e3ce8d2133582a96252fbeb4
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-03-21 17:40:16 +0900

    AlbumController 생성

---
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -0,0 +1,15 @@
+package com.squarecross.photoalbum.controller;
+
+import com.squarecross.photoalbum.dto.AlbumDto;
+import com.squarecross.photoalbum.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/albums")
+public class AlbumController {
+}
