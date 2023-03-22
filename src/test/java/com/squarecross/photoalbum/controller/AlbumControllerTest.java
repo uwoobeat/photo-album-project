12168326a8bf2170009cf682379f527d51c8d504
uwoobeat <desktop3711@g.hongik.ac.kr>
2023-03-22 23:59:34 +0900

    AlbumControllerTest 생성

---
+++ src/test/java/com/squarecross/photoalbum/controller/AlbumControllerTest.java
@@ -0,0 +1,22 @@
+package com.squarecross.photoalbum.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AlbumControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private AlbumControllerTest albumController;
+}
