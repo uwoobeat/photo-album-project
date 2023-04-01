3fd4c916628c7c454389972a4d4f6a11ada51d29
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-04-01 19:48:36 +0900

    패키지 추가

--- src/test/java/com/squarecross/photoalbum/controller/AlbumControllerTest.java
+++ src/test/java/com/squarecross/photoalbum/controller/AlbumControllerTest.java
@@ -1,5 +1,7 @@
 package com.squarecross.photoalbum.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
