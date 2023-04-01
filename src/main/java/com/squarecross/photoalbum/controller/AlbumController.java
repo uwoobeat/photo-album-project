e8ad70f9385c5889e3f709486fa7a5762cb9201f
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-04-01 19:50:52 +0900

    앨범 생성 컨트롤러 추가

--- src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
+++ src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -4,10 +4,9 @@ import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/albums")
@@ -22,4 +21,10 @@ public class AlbumController {
     public ResponseEntity<AlbumDto> getAlbumById(@PathVariable final Long id) {
         return ResponseEntity.ok(albumService.getAlbumById(id));
     }
+
+    @PostMapping(value = "")
+    public ResponseEntity<AlbumDto> createAlbum(@RequestBody final AlbumDto albumDto) throws IOException {
+        return ResponseEntity.ok(albumService.createAlbum(albumDto));
+    }
+
 }
