adf94ce6669f0cc09a25557a657b5d4b6aa6da1f
Jaehyun Ahn <desktop3711@g.hongik.ac.kr>
2023-04-04 16:23:48 +0900

    앨범 생성 POST 컨트롤러 테스트 추가

--- src/test/java/com/squarecross/photoalbum/controller/AlbumControllerTest.java
+++ src/test/java/com/squarecross/photoalbum/controller/AlbumControllerTest.java
@@ -1,5 +1,8 @@
 package com.squarecross.photoalbum.controller;
 
+import com.squarecross.photoalbum.domain.Album;
+import com.squarecross.photoalbum.dto.AlbumDto;
+import com.squarecross.photoalbum.mapper.AlbumMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,4 +27,23 @@ public class AlbumControllerTest {
 
     @Autowired
     private AlbumController albumController;
+
+    @Test
+    @DisplayName("앨범 생성 POST 테스트")
+    void createAlbum() {
+        // given
+        Album album = Album.createAlbum("testAlbum");
+        AlbumDto albumDto = AlbumMapper.toDto(album);
+
+        // when
+        ResponseEntity<AlbumDto> responseEntity = restTemplate.postForEntity("/api/v1/albums", albumDto, AlbumDto.class);
+
+        // then
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals("testAlbum", responseEntity.getBody().getName());
+        assertNotNull(responseEntity.getBody().getId());
+        assertNotNull(responseEntity.getBody().getCreatedAt());
+        assertEquals(0, responseEntity.getBody().getPhotoCount());
+    }
+
 }
