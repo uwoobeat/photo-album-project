566b6d3ab05fdfec50958f16d1c07da83f89aa49
Jaehyun Ahn <desktop3711@gmail.com>
2023-04-24 03:09:48 +0900

    키워드 포함 사진 검색 및 정렬 쿼리 메서드 추가

--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -4,6 +4,15 @@ import com.squarecross.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+    List<Photo> findByAlbumIdAndNameContainingOrderByCreatedAtAsc(Long id, String keyword);
+
+    List<Photo> findByAlbumIdAndNameContainingOrderByCreatedAtDesc(Long id, String keyword);
+
+    List<Photo> findByAlbumIdAndNameContainingOrderByNameAsc(Long id, String keyword);
+
+    List<Photo> findByAlbumIdAndNameContainingOrderByNameDesc(Long id, String keyword);
 }
