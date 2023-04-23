c833700f81ee5a38ac2b1a69082a99afdd7d3432
Jaehyun Ahn <desktop3711@gmail.com>
2023-04-24 03:08:00 +0900

    getPhotoList 컨트롤러 메서드 추가

--- src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
+++ src/main/java/com/squarecross/photoalbum/controller/PhotoController.java
@@ -1,8 +1,11 @@
 package com.squarecross.photoalbum.controller;
 
+import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.service.PhotoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/albums/{albumId}/photos")
@@ -11,5 +14,15 @@ public class PhotoController {
 
     public PhotoController(PhotoService photoService) {
         this.photoService = photoService;
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<List<PhotoDto>> getPhotoList(
+            @PathVariable("albumId") final Long id,
+            @RequestParam(value = "sort", required = false, defaultValue = "byDate") final String sort,
+            @RequestParam(value = "keyword", required = false, defaultValue = "") final String keyword,
+            @RequestParam(value = "orderBy", required = false, defaultValue = "desc") final String orderBy
+    ) {
+        return ResponseEntity.ok(photoService.getPhotoList(id, sort, keyword, orderBy));
     }
 }
