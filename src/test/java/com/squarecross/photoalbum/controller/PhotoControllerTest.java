83ce6090159b71550ed4b21f50d25fd7b24f0fa2
uwoobeat <desktop3711@gmail.com>
2023-04-28 22:31:08 +0900

    사진 목록 조회 컨트롤러 테스트 추가

--- src/test/java/com/squarecross/photoalbum/controller/PhotoControllerTest.java
+++ src/test/java/com/squarecross/photoalbum/controller/PhotoControllerTest.java
@@ -11,8 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -50,5 +54,30 @@ class PhotoControllerTest {
 
         // then
         assertEquals(200, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    @DisplayName("사진 목록 조회 GET 테스트")
+    void getPhotoList() {
+        // given
+        Album album = Album.createAlbum("testAlbum");
+        albumRepository.save(album);
+
+        for (int i = 0; i < 3; i++) {
+            PhotoDto photoDto = PhotoDto.builder()
+                    .fileName("testPhoto" + i)
+                    .fileSize(100L * i)
+                    .fileUrl("testFileUrl" + i)
+                    .thumbnailUrl("testThumbnailUrl" + i)
+                    .build();
+            restTemplate.postForEntity("/api/v1/albums/1/photos", photoDto, PhotoDto.class);
+        }
+
+        // when
+        ResponseEntity<List<PhotoDto>> responseEntity = restTemplate.exchange("/api/v1/albums/1/photos", HttpMethod.GET, null, new ParameterizedTypeReference<List<PhotoDto>>() {});
+
+        // then
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals(3, responseEntity.getBody().size());
     }
 }
